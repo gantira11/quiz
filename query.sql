e3d2cd15e30fcda219b68c70d18da30fb8088d38
Syaiful Fitrawan <syaiful.fitrawan@smt-id.com>
2024-03-12 01:53:12 +0700

    🗃️ db(Query): add create database query

--- query.sql
+++ query.sql
@@ -1,73 +1,95 @@
--- CREATE TABLE roles (
---     id BINARY(40) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
---     name VARCHAR(255) NOT NULL,
---     created_at TIMESTAMP DEFAULT NULL,
---     updated_at TIMESTAMP DEFAULT NULL,
---     deleted_at TIMESTAMP DEFAULT NULL
--- );
+-- Database Quiz
+CREATE DATABASE quiz;
 
--- CREATE TABLE users (
---     id BINARY PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
---     name VARCHAR(255) NOT NULL,
---     username VARCHAR(100) NOT NULL,
---     password VARCHAR(100) NOT NULL,
---     role_id BINARY(40),
---     FOREIGN KEY (role_id) REFERENCES roles(id),
---     created_at TIMESTAMP DEFAULT NULL,
---     updated_at TIMESTAMP DEFAULT NULL,
---     deleted_at TIMESTAMP DEFAULT NULL
--- );
+-- Tabel roles
+CREATE TABLE roles (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
 
--- CREATE TABLE subjects (
---     id BINARY(40) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
---     name VARCHAR(255) NOT NULL,
---     content TEXT NOT NULL,
---     created_at TIMESTAMP DEFAULT NULL,
---     updated_at TIMESTAMP DEFAULT NULL,
---     deleted_at TIMESTAMP DEFAULT NULL
--- );
+-- Tabel users
+CREATE TABLE users (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role_id CHAR(36),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    created_at TIMESTAMP DEFAULT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
 
--- CREATE TABLE quizzes (
---     id BINARY(40) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
---     name VARCHAR(255) NOT NULL,
---     subject_id BINARY(40),
---     FOREIGN KEY (subject_id) REFERENCES subjects(id),
---     created_at TIMESTAMP DEFAULT NULL,
---     updated_at TIMESTAMP DEFAULT NULL,
---     deleted_at TIMESTAMP DEFAULT NULL
--- );
+-- Tabel subjects
+CREATE TABLE subjects (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
 
--- CREATE TABLE quetions (
---     id BINARY(40) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
---     name VARCHAR(255) NOT NULL,
---     discuss TEXT DEFAULT NULL,
---     quiz_id BINARY(40),
---     FOREIGN KEY (quiz_id) REFERENCES quizzes(id),
---     created_at TIMESTAMP DEFAULT NULL,
---     updated_at TIMESTAMP DEFAULT NULL,
---     deleted_at TIMESTAMP DEFAULT NULL
--- );
+-- Tabel videos
+CREATE TABLE videos (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(255) NOT NULL,
+    file_url VARCHAR(255) NOT NULL,
+    subject_id CHAR(36),
+    FOREIGN KEY (subject_id) REFERENCES subjects(id),
+    created_at TIMESTAMP DEFAULT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
 
--- CREATE TABLE options (
---     id BINARY(40) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
---     name VARCHAR(255) NOT NULL,
---     is_correct BOOLEAN NOT NULL DEFAULT FALSE,
---     quetion_id BINARY(40),
---     FOREIGN KEY (quetion_id) REFERENCES quetions(id),
---     created_at TIMESTAMP DEFAULT NULL,
---     updated_at TIMESTAMP DEFAULT NULL,
---     deleted_at TIMESTAMP DEFAULT NULL
--- );
+-- Tabel quizzes
+CREATE TABLE quizzes (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(255) NOT NULL,
+    subject_id CHAR(36),
+    FOREIGN KEY (subject_id) REFERENCES subjects(id),
+    created_at TIMESTAMP DEFAULT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
 
--- CREATE TABLE answers (
---     id BINARY(40) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
---     quetions TEXT NOT NULL,
---     point DOUBLE NOT NULL,
---     quiz_id BINARY(40),
---     FOREIGN KEY (quiz_id) REFERENCES quizzes(id),
---     user_id BINARY(40),
---     FOREIGN KEY (user_id) REFERENCES users(id),
---     created_at TIMESTAMP DEFAULT NULL,
---     updated_at TIMESTAMP DEFAULT NULL,
---     deleted_at TIMESTAMP DEFAULT NULL
--- );
+-- Tabel questions
+CREATE TABLE questions (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(255) NOT NULL,
+    discuss TEXT DEFAULT NULL,
+    quiz_id CHAR(36),
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(id),
+    created_at TIMESTAMP DEFAULT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
+
+-- Tabel options
+CREATE TABLE options (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(255) NOT NULL,
+    is_correct BOOLEAN NOT NULL DEFAULT FALSE,
+    question_id CHAR(36),
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    created_at TIMESTAMP DEFAULT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
+
+-- Tabel answers
+CREATE TABLE answers (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    questions TEXT NOT NULL,
+    point DOUBLE NOT NULL,
+    quiz_id CHAR(36),
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(id),
+    user_id CHAR(36),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
