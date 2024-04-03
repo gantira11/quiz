11466de973fc0a5ee7ecad56a4048dea0b000ab2
Syaiful Fitrawan <syaiful.fitrawan@smt-id.com>
2024-04-03 23:18:56 +0700

    🚑 hotfix(Cors): add cors options

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,34 @@ import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as basicAuth from 'express-basic-auth';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+	app.use(function (req, res, next) {
+		req.headers.origin = req.headers.origin || req.headers.host;
+		next();
+	});
+
+	var whitelist = process.env.CORS_OPTION_ORIGIN.split(",");
+  const corsOptions = {
+		exposedHeaders: ['Content-Disposition'],
+    // 'origin': function (origin, callback) {
+    //   if (whitelist.indexOf(origin) !== -1) {
+    //       callback(null, true)
+    //   } else {
+    //     console.log("blocked cors for:", origin)
+    //     callback(new Error('Not allowed by CORS'))
+    //   }
+    // },
+    'methods': "GET,HEAD,PUT,PATCH,POST,DELETE",
+    'preflightContinue': false,
+    'optionsSuccessStatus': 204,
+    'credentials': true
+  }
+
+  app.enableCors(corsOptions);
 
   app.use(helmet());
 
