c007159f3aa79488b572d1d954026bea96fa0651
Syaiful Fitrawan <syaiful.fitrawan@smt-id.com>
2024-04-04 01:34:42 +0700

    📝 feat(Query): add alter query

--- query.sql
+++ query.sql
@@ -100,3 +100,6 @@ INSERT INTO roles (id, name, created_at, updated_at)
 VALUES 
 (UUID(), 'admin', NOW(), NOW()),
 (UUID(), 'student', NOW(), NOW());
+
+-- Add Column in Quizzes
+ALTER TABLE quizzes ADD category VARCHAR(255) NOT NULL
